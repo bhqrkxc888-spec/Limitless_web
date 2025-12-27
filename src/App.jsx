5deabef8a234d9f3f36f08648cf94c9f7d1345a8
bhqrkxc888-spec <bhqrkxc888@privaterelay.appleid.com>
2025-12-27 17:39:42 +0000

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate, useLocation } from 'react-router-dom'
 import { useEffect, lazy, Suspense } from 'react'
 import { analyzePageSEO } from './services/seoMonitoring'
 
